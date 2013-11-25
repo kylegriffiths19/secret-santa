ebc7d580ddb1960abf57e7ca2ab746f7626175ef
Kyle Griffiths <tha_griffmeister@live.co.uk>
2013-11-25 18:48:32 +0000

    php files added need tweaking

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -3,6 +3,8 @@ $(document).ready(function() {
 
    $(".btn-plus").on("click", function(e){
    	e.preventDefault();
+   	var count = $(".plus-input").val();
+   	alert(count);
    	$('<section class="row"><input type="text" name="name" value="Your Name*" onblur="if(this.value == \'\') { this.value=\'Your Name*\'}" onfocus="if (this.value == \'Your Name*\') {this.value=\'\'}"><input type="text" name="email" value="Your Email*"  onblur="if(this.value == \'\') { this.value=\'Your Email*\'}" onfocus="if (this.value == \'Your Email*\') {this.value=\'\'}"></section>').appendTo(".form");
    });
 
