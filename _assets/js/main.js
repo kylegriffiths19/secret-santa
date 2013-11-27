92a38b8d11a04bb175258e183230b57636ce50cf
Kyle Griffiths <tha_griffmeister@live.co.uk>
2013-11-27 15:07:55 +0000

    dynamically added number to name attributes

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -12,17 +12,18 @@ $("form").on("click", ".row .btn-remove", function(e){
 });
 
 
+
 //add rows
 $(".btn-plus").on("click", function(e){
 
 	e.preventDefault();
 
-	var count = $(".plus-input").val();
-   
-	for(var i = 0; i < count; i++) {
-        
-		$('<section class="row"><input type="text" name="name[]" placeholder="Name"><input type="text" name="email[]" placeholder="Email"><a href="#" class="btn btn-red btn-remove ss-icon">&#x002D;</a></section>').appendTo(".form form");
+    var count = $(".plus-input").val();
+    var name_counter = $(".row.input").length;
 
+	for(var i = 0; i < count; i++) {
+        name_counter++;
+		$('<section class="row input"><input type="text" name="name_'+name_counter+'" placeholder="Name"><input type="text" name="email[]" placeholder="Email"><a href="#" class="btn btn-red btn-remove ss-icon">&#x002D;</a></section>').appendTo(".form form");
 	}
 });
 
